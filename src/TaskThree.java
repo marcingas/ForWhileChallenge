21430486fe9d5e4073622e8cb1bb364fda012a88
Marcin <gasiorek.mail@gmail.com>
2023-03-15 12:54:50 +0100

    challenge 3 repair

--- src/TaskThree.java
+++ src/TaskThree.java
@@ -1,6 +1,5 @@
 import java.util.Arrays;
 import java.util.Scanner;
-import java.util.jar.JarOutputStream;
 
 public class TaskThree {
     public static void main(String[] args) {
@@ -24,15 +23,16 @@ public class TaskThree {
                 }
 
 //                use while:
-//                int i = 0;
-//                while (i< word.length) {
-//                    if (word[i].equals("a") || word[i].equals("e") || word[i].equals("i") || word[i].equals("o") || word[i].equals("u") || word[i].equals("y")) {
-//                        System.out.println("podane słowo: " + "-" + input + "-" + " ma więcej niż 3 litery i ma przynajmniej jedną samogłoskę: " + word[i]);
-//                        flag = false;
-//                        break;
-//                    }
-//                    i++;
-//                }
+                System.out.println("----while-----");
+                int i = 0;
+                while (i< word.length) {
+                    if (word[i].equals("a") || word[i].equals("e") || word[i].equals("i") || word[i].equals("o") || word[i].equals("u") || word[i].equals("y")) {
+                        System.out.println("podane słowo: " + "-" + input + "-" + " ma więcej niż 3 litery i ma przynajmniej jedną samogłoskę: " + word[i]);
+                        flag = false;
+                        break;
+                    }
+                    i++;
+                }
             }
         }
     }
